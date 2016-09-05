e2ea55748629304f2182ada0a96ac421e2ba1890
Marko Stojanović <mstojanovic@maxeler.com>
2016-09-05 16:19:47 +0200

    Adding preprocessing FFT: CPU code

--- CPUCode/PoissonCpuCode.c
+++ CPUCode/PoissonCpuCode.c
@@ -12,7 +12,6 @@
 #define MD Poisson_M
 #define LD Poisson_L
 
-
 /**
  * Wrapper for malloc in order to check if malloc returns NULL and exit if this happens.
  *
@@ -117,6 +116,186 @@ double timesec() {
 }
 
 /**
+ * CPU code to calculate a 1D fft.
+ * This function is not written to be fast. Only to be simple.
+ *
+ * @param n Size of the fft (has to be power of 2).
+ * @param values Input samples. Also used to store the coefficients.
+ */
+void fftCPU(const int n, float complex* values) {
+	if (n > 1) {
+		float complex *g = (float complex*) mallocWrapper((n / 2) * sizeof(float complex));
+		float complex *u = (float complex*) mallocWrapper((n / 2) * sizeof(float complex));
+
+
+		// reorder data
+		for (int i = 0; i < n / 2; i++) {
+			g[i] = values[i * 2];
+			u[i] = values[i * 2 + 1];
+		}
+
+		// calculate fft recursively
+		fftCPU(n / 2, g);
+		fftCPU(n / 2, u);
+
+		// combine results
+		for (int k = 0; k < n / 2; k++) {
+			float complex expFactor = cexpf(-2.0 * M_PI * I * (float complex) k / (float complex) n);
+			values[k]         = g[k] + u[k] * expFactor;
+			values[k + n / 2] = g[k] - u[k] * expFactor;
+		}
+
+		// free allocated memory
+		free(g);
+		free(u);
+	}
+}
+
+/**
+ * Function to transpose a 2D Array.
+ *
+ * @param firstDimension Size of the first dimension.
+ * @param secondDimension Size of the second dimension.
+ * @param data The array to transpose. It will be replaced by the transposed array.
+ */
+void transposeData(const int firstDimension, const int secondDimension, float complex*** data) {
+	float complex** newData = mallocWrapper(firstDimension * sizeof(float complex*));
+	for (int i = 0; i < firstDimension; i++) {
+		newData[i] = mallocWrapper(secondDimension * sizeof(float complex));
+		for (int j = 0; j < secondDimension; j++) {
+			newData[i][j] = (*data)[j][i];
+		}
+	}
+
+	// free space again
+	for (int i = 0; i < secondDimension; i++) {
+		free((*data)[i]);
+	}
+	free(*data);
+	*data = newData;
+}
+
+/**
+ * Wrapper function for the CPU implementation of the 1D fft.
+ * In the 1D case the input data gets copied into a new array in order to do not change the input.
+ * In the 2D case the data has to be reordered, transposed and multiple 1D ffts have to be executed.
+ * The same principle applies for the 3D case
+ *
+ * @param size number of samples.
+ * @param inputData Sample array.
+ * @param expectedData Array for the coefficients.
+ */
+void fftCPUWrapper(const int size, float complex* inputData, float complex* expectedData) {
+	if (MD == 1) { // 1D FFT
+		memcpy((void*) expectedData, (void*) inputData, size * sizeof(float complex));
+		fftCPU(size, expectedData);
+	} else if (LD == 1) { // 2D FFT
+		// To make things clearer again we copy our data into a two dimensional array
+		float complex** inputData2D = mallocWrapper(MD * sizeof(float complex*));
+		for (int i = 0; i < MD; i++) {
+			inputData2D[i] = mallocWrapper(ND * sizeof(float complex));
+			for (int j = 0; j < ND; j++) {
+				inputData2D[i][j] = inputData[i * ND + j];
+			}
+		}
+
+		// Calculate fft on each row
+		for (int i = 0; i < MD; i++) {
+			fftCPU(ND, inputData2D[i]);
+		}
+
+		// transpose
+		transposeData(ND, MD, &inputData2D);
+
+		// calculate fft on each column
+		for (int i = 0; i < ND; i++) {
+			fftCPU(MD, inputData2D[i]);
+		}
+
+		// transpose back
+		transposeData(MD, ND, &inputData2D);
+
+		for (int i = 0; i < MD; i++) {
+			for (int j = 0; j < ND; j++) {
+				expectedData[i * ND + j] = inputData2D[i][j];
+			}
+		}
+
+		for (int i = 0; i < MD; i++) {
+			free(inputData2D[i]);
+		}
+		free(inputData2D);
+	} else { // 3D FFT
+		// To make things clearer again we copy our data into a three dimensional Array
+		float complex*** inputData3D = mallocWrapper(LD * sizeof(float complex**));
+		for (int i = 0; i < LD; i++) {
+			inputData3D[i] = mallocWrapper(MD * sizeof(float complex*));
+			for (int j = 0; j < MD; j++) {
+				inputData3D[i][j] = mallocWrapper(ND * sizeof(float complex));
+				for (int k = 0; k < ND; k++) {
+					inputData3D[i][j][k] = inputData[i * ND * MD + j * ND + k];
+				}
+			}
+		}
+
+		// Now we can first calculate the fft on each row
+		for (int i = 0; i < LD; i++) {
+			for (int j = 0; j < MD; j++) {
+				fftCPU(ND, inputData3D[i][j]);
+			}
+		}
+
+		// Transpose N and M Dimension
+		for (int i = 0; i < LD; i++) {
+			transposeData(ND, MD, &(inputData3D[i]));
+		}
+
+		// Calculate fft on each column
+		for (int i = 0; i < LD; i++) {
+			for (int j = 0; j < ND; j++) {
+				fftCPU(MD, inputData3D[i][j]);
+			}
+		}
+
+		// Transpose N and M Dimension back
+		for (int i = 0; i < LD; i++) {
+			transposeData(MD, ND, &(inputData3D[i]));
+		}
+
+		// Now do fft in the L Dimension
+		float complex* buffer = mallocWrapper(LD * sizeof(float complex));
+		for (int i = 0; i < MD; i++) {
+			for (int j = 0; j < ND; j++) {
+				for (int k = 0; k < LD; k++) {
+					buffer[k] = inputData3D[k][i][j];
+				}
+				fftCPU(LD, buffer);
+				for (int k = 0; k < LD; k++) {
+					inputData3D[k][i][j] = buffer[k];
+				}
+			}
+		}
+		free(buffer);
+
+		for (int i = 0; i < LD; i++) {
+			for (int j = 0; j < MD; j++) {
+				for (int k = 0; k < ND; k++) {
+					expectedData[i * ND * MD + j * ND + k] = inputData3D[i][j][k];
+				}
+			}
+		}
+
+		for (int i = 0; i < LD; i++) {
+			for (int j = 0; j < MD; j++) {
+				free(inputData3D[i][j]);
+			}
+			free(inputData3D[i]);
+		}
+		free(inputData3D);
+	}
+}
+
+/**
  * CPU code to solve Poisson equation.
  *
  * @param values Input samples.
@@ -142,8 +321,9 @@ void poissonCPU(float complex* input, double complex* contents, float complex* e
  *
  */
 void poissonCPUWrapper(const int size, float complex* inputData, double complex* contents, float complex* expectedData) {
-	//fftCPUWrapper(size, inputData, expectedData);
-	poissonCPU(inputData,contents,expectedData);
+	fftCPUWrapper(size, inputData, expectedData);
+	poissonCPU(expectedData,contents,expectedData);
+	//poissonCPU(inputData,contents,expectedData);
 	//ifftCPUWrapper(size, expectedData, expectedData);
 }
 
@@ -158,7 +338,7 @@ void poissonDFE(const int size, float complex* input, double complex* contents, 
 	printf("Running on DFE.\n");
 	float h=1/(float)ND;
 	float dh = h * h;
-	Poisson(size / 4, dh, input, size * sizeof(float complex), result, size * sizeof(float complex), (double*)contents);
+	Poisson(size / 4, size / 4, dh, input, size * sizeof(float complex), result, size * sizeof(float complex), (double*)contents);
 }
 
 int main(void)
